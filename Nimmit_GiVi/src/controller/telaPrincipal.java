2df2725ea809d032cca429f52f23954179143455
violet657 <vs.3809@gmail.com>
2023-11-28 11:35:37 -0300

    Arquivo tela principal

---
+++ Nimmit_GiVi/src/controller/telaPrincipal.java
@@ -0,0 +1,268 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class telaPrincipal {
+
+    @FXML
+    private ImageView crt_00;
+
+    @FXML
+    private ImageView crt_01;
+
+    @FXML
+    private ImageView crt_02;
+
+    @FXML
+    private ImageView crt_03;
+
+    @FXML
+    private ImageView crt_04;
+
+    @FXML
+    private ImageView crt_10;
+
+    @FXML
+    private ImageView crt_11;
+
+    @FXML
+    private ImageView crt_12;
+
+    @FXML
+    private ImageView crt_13;
+
+    @FXML
+    private ImageView crt_14;
+
+    @FXML
+    private ImageView crt_20;
+
+    @FXML
+    private ImageView crt_21;
+
+    @FXML
+    private ImageView crt_22;
+
+    @FXML
+    private ImageView crt_23;
+
+    @FXML
+    private ImageView crt_24;
+
+    @FXML
+    private ImageView crt_30;
+
+    @FXML
+    private ImageView crt_31;
+
+    @FXML
+    private ImageView crt_32;
+
+    @FXML
+    private ImageView crt_33;
+
+    @FXML
+    private ImageView crt_34;
+
+    @FXML
+    private ImageView crt_40;
+
+    @FXML
+    private ImageView crt_41;
+
+    @FXML
+    private ImageView crt_42;
+
+    @FXML
+    private ImageView crt_43;
+
+    @FXML
+    private ImageView crt_44;
+
+    @FXML
+    private ImageView crttopo_00;
+
+    @FXML
+    private ImageView crttopo_01;
+
+    @FXML
+    private ImageView crttopo_02;
+
+    @FXML
+    private ImageView crttopo_03;
+
+    @FXML
+    private ImageView crttopo_04;
+
+    @FXML
+    private ImageView crttopo_05;
+
+    @FXML
+    private ImageView crttopo_10;
+
+    @FXML
+    private ImageView crttopo_11;
+
+    @FXML
+    private ImageView crttopo_12;
+
+    @FXML
+    private ImageView crttopo_13;
+
+    @FXML
+    private ImageView crttopo_14;
+
+    @FXML
+    private ImageView crttopo_15;
+
+    @FXML
+    private Text lbl_00;
+
+    @FXML
+    private Text lbl_01;
+
+    @FXML
+    private Text lbl_02;
+
+    @FXML
+    private Text lbl_03;
+
+    @FXML
+    private Text lbl_04;
+
+    @FXML
+    private Text lbl_10;
+
+    @FXML
+    private Text lbl_11;
+
+    @FXML
+    private Text lbl_12;
+
+    @FXML
+    private Text lbl_13;
+
+    @FXML
+    private Text lbl_14;
+
+    @FXML
+    private Text lbl_20;
+
+    @FXML
+    private Text lbl_21;
+
+    @FXML
+    private Text lbl_22;
+
+    @FXML
+    private Text lbl_23;
+
+    @FXML
+    private Text lbl_24;
+
+    @FXML
+    private Text lbl_30;
+
+    @FXML
+    private Text lbl_31;
+
+    @FXML
+    private Text lbl_32;
+
+    @FXML
+    private Text lbl_33;
+
+    @FXML
+    private Text lbl_34;
+
+    @FXML
+    private Text lbl_40;
+
+    @FXML
+    private Text lbl_41;
+
+    @FXML
+    private Text lbl_42;
+
+    @FXML
+    private Text lbl_43;
+
+    @FXML
+    private Text lbl_44;
+
+    @FXML
+    private Text lbltopo_00;
+
+    @FXML
+    private Text lbltopo_01;
+
+    @FXML
+    private Text lbltopo_02;
+
+    @FXML
+    private Text lbltopo_03;
+
+    @FXML
+    private Text lbltopo_04;
+
+    @FXML
+    private Text lbltopo_05;
+
+    @FXML
+    private Text lbltopo_10;
+
+    @FXML
+    private Text lbltopo_11;
+
+    @FXML
+    private Text lbltopo_12;
+
+    @FXML
+    private Text lbltopo_13;
+
+    @FXML
+    private Text lbltopo_14;
+
+    @FXML
+    private Text lbltopo_15;
+
+    @FXML
+    private TextField playername1;
+
+    @FXML
+    private TextField playername2;
+
+    @FXML
+    private TextField playername3;
+
+    @FXML
+    private TextField playername4;
+
+    @FXML
+    private TextField playername5;
+
+    @FXML
+    private TextField playername6;
+
+    @FXML
+    private TextField playerpoints1;
+
+    @FXML
+    private TextField playerpoints2;
+
+    @FXML
+    private TextField playerpoints3;
+
+    @FXML
+    private TextField playerpoints4;
+
+    @FXML
+    private TextField playerpoints5;
+
+    @FXML
+    private TextField playerpoints6;
+
+}
+
