f21b793012428c48c8612115f70bd9b0d504e509
violet657 <vs.3809@gmail.com>
2023-11-30 12:09:02 -0300

    Adicionei a classe necessaria para usar o MouseEvent

--- Nimmit_GiVi/src/controller/MainScreenController.java
+++ Nimmit_GiVi/src/controller/MainScreenController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import model.Carta;
 import model.Player;
